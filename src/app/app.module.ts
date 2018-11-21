c6f45358e62a37b21dfc7889d9edea9ae47a0656
Yash Kumar Prajapati <yashkumar.prajapati@gmail.com>
2018-11-21 11:44:25 +0530

    Created Pages and ace-admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { RegisterComponent } from './home/register/register.component';
+import { LoginComponent } from './home/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
