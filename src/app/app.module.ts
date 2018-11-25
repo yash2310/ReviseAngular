ec3060899f58904480cfe46149fd2d6aedb29c33
Yash Kumar Prajapati <yashkumar.prajapati@gmail.com>
2018-11-25 14:40:36 +0530

    Project Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './home/register/register.component';
-import { LoginComponent } from './home/login/login.component';
-import { RoutesService } from './routes.service';
-import { AccountComponent } from './account/account.component';
-import { UserComponent } from './account/user/user.component';
-import { AdminComponent } from './account/admin/admin.component';
-import { HomeHeaderComponent } from './Components/home-header/home-header.component';
-import { AccountHeaderComponent } from './Components/account-header/account-header.component';
-import { FooterComponent } from './Components/footer/footer.component';
-import { LeftNavComponent } from './Components/left-nav/left-nav.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatInputModule} from '@angular/material';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { RegisterComponent } from "./home/register/register.component";
+import { LoginComponent } from "./home/login/login.component";
+import { RoutesService } from "./routes.service";
+import { AccountComponent } from "./account/account.component";
+import { UserComponent } from "./account/user/user.component";
+import { AdminComponent } from "./account/admin/admin.component";
+import { HomeHeaderComponent } from "./Components/home-header/home-header.component";
+import { AccountHeaderComponent } from "./Components/account-header/account-header.component";
+import { FooterComponent } from "./Components/footer/footer.component";
+import { LeftNavComponent } from "./Components/left-nav/left-nav.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatInputModule
+} from "@angular/material";
+import { MatDividerModule } from "@angular/material/divider";
+import { ForgetComponent } from "./home/forget/forget.component";
 
 @NgModule({
   declarations: [
@@ -31,16 +36,19 @@ import {MatButtonModule, MatCheckboxModule, MatInputModule} from '@angular/mater
     HomeHeaderComponent,
     AccountHeaderComponent,
     FooterComponent,
-    LeftNavComponent
+    LeftNavComponent,
+    ForgetComponent
   ],
   imports: [
     BrowserModule,
+    RoutesService,
     AppRoutingModule,
     RoutesService,
     BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule,
-    MatInputModule
+    MatInputModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routes.service.ts
+++ src/app/routes.service.ts
@@ -1,11 +1,12 @@
-import { Injectable, NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './home/login/login.component';
-import { RegisterComponent } from './home/register/register.component';
-import { AccountComponent } from './account/account.component';
-import { AdminComponent } from './account/admin/admin.component';
-import { UserComponent } from './account/user/user.component';
+import { Injectable, NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./home/login/login.component";
+import { RegisterComponent } from "./home/register/register.component";
+import { AccountComponent } from "./account/account.component";
+import { AdminComponent } from "./account/admin/admin.component";
+import { UserComponent } from "./account/user/user.component";
+import { ForgetComponent } from "./home/forget/forget.component";
 
 // @Injectable({
 //   providedIn: 'root'
@@ -16,7 +17,8 @@ const routes: Routes = [
     component: HomeComponent,
     children: [
       { path: "login", component: LoginComponent },
-      { path: "register", component: RegisterComponent }
+      { path: "register", component: RegisterComponent },
+      { path: "forgetpassword", component: ForgetComponent }
     ]
   },
   {
@@ -33,9 +35,6 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
 export class RoutesService {
-
-  constructor() { }
-
+  constructor() {}
 }
